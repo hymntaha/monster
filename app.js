e02867b3429c81fbb67199e4e667212ff2347d3b
Taha Uygun <tuygun@bluechipww.com>
2018-07-23 22:33:05 -0500

    adding special attack and keep refactoring

--- app.js
+++ app.js
@@ -13,12 +13,17 @@ new Vue({
        },
         attack: function(){
            this.monsterHealth -= this.calculateDamage(3,10);
-
-            if (this.checkWin()) {
-                return;
-            }
-           this.playerHealth -= this.calculateDamage(5, 12);
-           this.checkWin();
+           if (this.checkWin()) { return; }
+            this.monsterAttacks();
+        },
+        specialAttack: function(){
+            this.monsterHealth -= this.calculateDamage(10,20);
+            if (this.checkWin()) { return; }
+            this.monsterAttacks();
+        },
+        monsterAttacks: function(){
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
         },
         heal: function(){
 
